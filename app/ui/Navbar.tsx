6bc3b05e66beda805cde01c2ddc1028f7d1e8df9
JheisonVR <daviid.vargasr@gmail.com>
2023-11-14 12:36:47 -0500

    feat: The initial design of the home page

---
+++ app/ui/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <main className='my-6'>
+      <div className="flex flex-col">
+        <h1 className="text-white text-5xl font-bold   ">Roadmap</h1>
+        <h3 className="text-white underline my-3" >By Jheyson Vargas. Bogotá </h3>
+      </div>
+    </main>
+  );
+};
